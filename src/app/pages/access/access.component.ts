4e05576976fdc575cc247e3620f9200336f054e6
Dmitry Streltsov <dev-0x15@yandex.by>
2017-11-08 10:47:16 +0300

    backup

---
+++ src/app/pages/access/access.component.ts
@@ -0,0 +1,28 @@
+﻿import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AlertService } from '../../services/alert.service';
+
+@Component({
+    selector: 'access',
+    templateUrl: './access.component.html',
+    styleUrls: ['./access.component.css']
+})
+export class AccessComponent {
+
+  constructor(private router: Router, private alertService: AlertService) { }
+
+  access(code) {
+
+    if (!code) {
+
+      this.alertService.error("Няправільны код доступу");
+
+      return;
+    }
+
+    localStorage.setItem('key', code);
+
+    this.router.navigateByUrl('/');
+  }
+}
---
+++ src/app/pages/main/main.component.ts
@@ -0,0 +1,20 @@
+﻿import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'main',
+    templateUrl: './main.component.html',
+    styleUrls: ['./main.component.css']
+})
+export class MainComponent {
+
+  constructor(private router: Router) {
+
+    if (!localStorage.getItem('key')) {
+
+      this.router.navigateByUrl('/access');
+      return;
+    }
+  }
+
+}
